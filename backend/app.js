828ca1b16b0a1f304a39043c07bb1a393de7c24c
Isolutiontraders <139568173+Isolutiontraders@users.noreply.github.com>
2023-07-14 20:17:52 +0500

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 
 app.use(cors({
-  origin: ['https://isolutiontraders.vercel.app/'],
+  origin: ['https://isolutiontraders.vercel.app'],
   credentials: true,
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }));
